5c1e8d46e4acb6a95c065bac7a63767a5871e8d2
Prashant <prashant.bharambe@syntrosys.com>
2018-07-16 21:40:25 +0530

    Recipe and shopping list project

--- demo-project/src/app/recipes/recipe-list/recipe-list.component.ts
+++ demo-project/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,11 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Test Recipe 1', 'This is test recipe 1', 'https://i0.wp.com/palatablepastime.com/wp-content/uploads/2017/11/american-goulash-wide.jpg?fit=1024%2C733&ssl=1'),
+    new Recipe('Test Recipe 2', 'This is test recipe 2', 'https://upload.wikimedia.org/wikipedia/commons/7/79/Goulash_from_usa.jpg')
+  ];
 
   constructor() { }
 
